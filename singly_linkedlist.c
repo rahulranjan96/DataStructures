6c12b0b5e5e81c240faf2f6015c111e50e2d0edd
rahulranjan96 <rahulranjanyadav96@gmail.com>
2016-05-20 12:38:18 +0530

    Basic Singly Linkedlist

---
+++ singly_linkedlist.c
@@ -0,0 +1,55 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct Node
+{
+  int key;
+  struct Node *next;
+}node;
+
+void insert(node *a,int x)
+{
+ node *b = (node*)malloc(sizeof(node));
+ b->next=NULL;
+ b->key=x;
+ while(a->next!=NULL)
+   a=a->next;
+ a->next=b;
+}
+
+void print(node *a)
+{
+ while(a->next!=NULL)
+{ 
+  printf("%d ",a->next->key);
+  a=a->next;
+}
+}
+
+int main()
+{
+node *a=(node*)malloc(sizeof(node));
+a->key=0;
+a->next=NULL;
+int x,i;
+while(1)
+{
+printf("1:Insert\n2:Print\n3:Exit\nEnter Choice:");
+scanf("%d",&i);
+switch(i)
+{
+ case 1: printf("Enter the element to be inserted:");
+         scanf("%d",&x);
+         insert(a,x);
+         break;
+ case 2: print(a);
+         printf("\n");
+         break;
+ case 3: return 0;
+         break;
+
+}
+
+}
+
+}
