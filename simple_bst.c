a63b6ed7dd15518a9e90a7a10a6f145b584790d3
rahulranjan96 <rahulranjanyadav96@gmail.com>
2016-06-09 14:55:36 +0530

    successor and predecessor functions added

--- simple_bst.c
+++ simple_bst.c
@@ -5,20 +5,27 @@ typedef struct Node{
 	int key;
 	struct Node *left;
 	struct Node *right;
+        struct Node *parent;
 }node;
 
-void max(node *root)
+node* max(node *root)
 {
 	if(root->right!=NULL)
 		max(root->right);
-	else printf("%d\n",root->key);
+	else {
+              printf("%d\n",root->key);
+              return root;
+             }
 }
 
-void min(node *root)
+node* min(node *root)
 {
 	if(root->left!=NULL)
 		min(root->left);
-	else printf("%d\n",root->key);
+	else {
+               printf("%d\n",root->key);
+               return root;
+             }
 }
 
 void inorder(node *root)
@@ -28,7 +35,7 @@ void inorder(node *root)
 			inorder(root->left);
 			printf("%d ",root->key);
 			inorder(root->right);
-        }
+                } 
 }
 
 void preorder(node *root)
@@ -53,12 +60,15 @@ void postorder(node *root)
 }
 
 
-void search(node *root,int key)
+node* search(node *root,int key)
 {
 	if(root==NULL)
 		printf("Not Found\n");
 	else if(root->key==key)
-		printf("Found\n");
+		{
+                  printf("Found\n");
+                  return root;
+                }
 	else if(key<root->key)
 		search(root->left,key);
 	else if(key>root->key)
@@ -74,21 +84,68 @@ node* insert(node *root,int key)
 		root->key=key;
 		root->left=NULL;
 		root->right=NULL;
+                root->parent=NULL;
 		return root;
 	}
-	else if(key>=root->key)
-		root->right=insert(root->right,key);
-	else root->left=insert(root->left,key);
+	else if(key>root->key)
+		{
+                 root->right=insert(root->right,key);
+                 root->right->parent=root;
+                }
+	else {
+              root->left=insert(root->left,key);
+              root->left->parent=root;
+            }
 	return root;
 }
 
-	int main()
+void successor(node *root,int key)
+{
+  node *ans=NULL;
+  node *a=search(root,key);
+  if(a->right!=NULL)
+   ans=min(a->right);
+ else
+ {
+   node *b=a->parent;
+   while(b!=NULL&&a==b->right)
+      {
+        a=b;
+        b=a->parent;
+      }
+   ans=b;
+ }
+  if(ans!=NULL)
+    printf("%d\n",ans->key);
+}
+
+void predecessor(node *root,int key)
+{
+ node *ans=NULL;
+ node *a=search(root,key);
+ if(a->left!=NULL)
+  ans=max(a->left);
+ else
+{
+  node *b=a->parent;
+  while(b!=NULL&&a==b->left)
+      {
+        a=b;
+        b=a->parent;
+      }
+ ans=b;
+}
+  if(ans!=NULL)
+    printf("%d\n",ans->key);
+}
+
+int main()
 {
    node *root = NULL;
    int c,key;
    while(1)
    {
-   	printf("1:Insert\n2:Search\n3:Maximum\n4:Minimum\n5:Inorder\n6:Pre-Order\n7:Post-Order\n8:Exit\nEnter Choice:");
+   	printf("1:Insert\n2:Search\n3:Maximum\n4:Minimum\n5:Inorder\n6:Pre-Order\n7:Post-Order\n8:Successor\n9:Predecessor\n10:Exit\nEnter Choice:");
     scanf("%d",&c);
     switch(c)
     {
@@ -117,8 +174,18 @@ node* insert(node *root,int key)
       case 7:postorder(root);
              printf("\n");
              break;
-      case 8:return 0;
+      case 8:printf("Enter the key whose successor is to be found:");
+             scanf("%d",&key);
+             successor(root,key);
+             break;
+      case 9:printf("Enter the key whose predecessor is to be found:");
+              scanf("%d",&key);
+              predecessor(root,key);
+              break;
+     case 10:return 0;
+
       default:printf("Enter a valid choice\n");
+              break;
     }
     
    }
